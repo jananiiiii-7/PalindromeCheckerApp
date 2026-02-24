aa9784be3e1417a7010a4bededab658848e635ed
jananiiiii-7 <jananisreed@gmail.com>
2026-02-24 16:37:03 +0530

    UC10: Normalized Palindrome Validation

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,30 +1,27 @@
-public class UseCase9PalindromeCheckerApp {
+public class UseCase10PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        String input = "madam";
+        String input = "A man, a plan, a canal, Panama";
 
-        boolean result = check(input, 0, input.length() - 1);
+        String normalized = input.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
 
-        if (result) {
+        boolean isPalindrome = true;
+
+        for (int i = 0; i < normalized.length() / 2; i++) {
+
+            if (normalized.charAt(i) != normalized.charAt(normalized.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+
+        }
+
+        if (isPalindrome) {
             System.out.println(input + " is a palindrome");
         } else {
             System.out.println(input + " is not a palindrome");
         }
-
-    }
-
-    private static boolean check(String a, int start, int end) {
-
-        if (start >= end) {
-            return true;
-        }
-
-        if (a.charAt(start) != a.charAt(end)) {
-            return false;
-        }
-
-        return check(a, start + 1, end - 1);
 
     }
 
