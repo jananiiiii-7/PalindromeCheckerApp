2d2a85512c57e1f9b9543a9227b5578fcab9c0a3
jananiiiii-7 <jananisreed@gmail.com>
2026-02-24 15:21:17 +0530

    UC1: Application Entry & Welcome Message

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,13 @@
+public class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("==================================");
+        System.out.println("      PALINDROME CHECKER APP      ");
+        System.out.println("==================================");
+
+        System.out.println("Application Name : Palindrome Checker App");
+        System.out.println("Application Version : 1.0");
+
+    }
+}
